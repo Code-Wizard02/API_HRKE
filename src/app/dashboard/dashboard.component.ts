7c47c0d980d093c97fd9b74bae8110717c8cb4fe
Kevin Hernández <21160665@itoaxaca.edu.mx>
2024-11-20 11:23:58 -0600

    Logout Button is Working

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,11 +2,12 @@ import {Component, Input} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatToolbarModule],
+  imports: [MatIconModule, MatButtonModule, MatToolbarModule, MatMenuModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
@@ -14,4 +15,8 @@ export class DashboardComponent {
   @Input() userName: string = '';
   @Input() userAvatar: string = '';
   
+  logout() {
+    localStorage.removeItem('token');
+    window.location.reload();
+  }
 }
